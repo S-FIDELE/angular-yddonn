4d426dd37f067e937c421c9f4b1aa66e976e8b28
S-FIDELE <121621483+S-FIDELE@users.noreply.github.com>
2023-05-09 22:31:47 +0200

    1 file modified

--- src/main.ts
+++ src/main.ts
@@ -4,18 +4,31 @@ import { CommonModule } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
 
 @Component({
-  selector: 'my-app',
-  standalone: true,
-  imports: [CommonModule],
-  template: `
-    <h1>Hello from {{name}}!</h1>
-    <a target="_blank" href="https://angular.io/start">
-      Learn more about Angular 
-    </a>
-  `,
-})
-export class App {
-  name = 'Angular';
-}
-
-bootstrapApplication(App);
+  selector: 'app-todo-list',
+  template: <h1>My Todo List</h1> <ul> <li *ngFor="let todo of todos">{{ todo.title }}</li> </ul> })
+  export class TodoListComponent {
+  todos: any[];
+  
+  constructor(private todoService: TodoService) {}
+  
+  ngOnInit() {
+  this.todos = this.todoService.getTodos();
+  }
+  }
+  
+  import { Injectable } from '@angular/core';
+  
+  @Injectable({
+  providedIn: 'root'
+  })
+  export class TodoService {
+  private todos = [
+  { title: 'Buy milk', completed: false },
+  { title: 'Go to gym', completed: true },
+  { title: 'Learn Angular', completed: false }
+  ];
+  
+  getTodos() {
+  return this.todos;
+  }
+  }
